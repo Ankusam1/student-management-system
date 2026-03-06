62e9a18f4382b075ad6eeec6ef408c0fb8c49497
Ankit Samal <ankitsamal966@gmail.com>
2026-03-06 22:21:06 +0530

    Prepare project for deployment

--- smsproject/settings.py
+++ smsproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4y2@t1#^351&&7l4vgiol@#(eona^@@(1xpv%z7k+7ar&6(ewe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
