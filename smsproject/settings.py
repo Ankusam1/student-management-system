fa9a1daefed0345ab0ca131c034ed60cf2f9bed2
Ankit Samal <ankitsamal966@gmail.com>
2026-03-14 15:01:35 +0530

    fix login redirect

--- smsproject/settings.py
+++ smsproject/settings.py
@@ -131,9 +131,7 @@ USE_TZ = True
 # DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-LOGIN_URL = 'login'
-LOGIN_REDIRECT_URL = 'home'
-LOGOUT_REDIRECT_URL = 'login'
+
 
 STATIC_URL = '/static/'
 
@@ -142,3 +140,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/login/'
